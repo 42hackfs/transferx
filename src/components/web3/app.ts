7d2a32b06c295169aa953a85676a36e7956087ea
Fredrika Lindh <fredrikalindh@gmail.com>
2021-08-03 09:34:36 +0200

    manual merge

---
+++ src/components/web3/app.ts
@@ -0,0 +1,46 @@
+import { DID } from 'dids';
+import ThreeIdResolver from '@ceramicnetwork/3id-did-resolver';
+import KeyDidResolver from 'key-did-resolver';
+import type { ResolverRegistry } from 'did-resolver';
+
+import { createCeramic } from './ceramic';
+import { createIDX } from './idx';
+import { getProvider } from './wallet';
+
+declare global {
+  interface Window {
+    did?: DID;
+  }
+}
+
+const ceramicPromise = createCeramic();
+
+const authenticate = async (): Promise<string> => {
+  const [ceramic, provider] = await Promise.all([ceramicPromise, getProvider()]);
+  const keyDidResolver = KeyDidResolver.getResolver();
+  const threeIdResolver = ThreeIdResolver.getResolver(ceramic);
+  const resolverRegistry: ResolverRegistry = {
+    ...threeIdResolver,
+    ...keyDidResolver
+  };
+  const did = new DID({
+    provider,
+    resolver: resolverRegistry
+  });
+  await did.authenticate();
+  await ceramic.setDID(did);
+  const idx = createIDX(ceramic);
+  window.did = ceramic.did;
+  return idx.id;
+};
+
+document.getElementById('bauth')?.addEventListener('click', () => {
+  authenticate().then(
+    (id) => {
+      console.log('Connected with DID:', id);
+    },
+    (err) => {
+      console.error('Failed to authenticate:', err);
+    }
+  );
+});
