0a5be81ae1ef186039a66eabcc3b068db6ab9d69
Mathis <mathis.grosjean.42@gmail.com>
2021-08-05 11:23:52 +0200

    web3 storage

--- src/App.tsx
+++ src/App.tsx
@@ -17,10 +17,27 @@ import React from "react";
 import LoadingScreen from "./components/LoadingScreen";
 import Container from "@material-ui/core/Container";
 import Typography from "@material-ui/core/Typography";
+import { Web3Storage } from "web3.storage";
+
+function getAccessToken() {
+  // If you're just testing, you can paste in a token
+  // and uncomment the following line:
+  return "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDBEZkFlODM5QzllMDE0ZTVkN2VBNjQ3RkIxQ2Q3ZjZkOUEwN2M1ZTUiLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2MjgwMTAyOTI1OTMsIm5hbWUiOiJtYXRoaXMifQ.DKiUfTgLAUufFweDwZiJKqvS1vdBD3_-sd4c3-mUCaY";
+
+  // In a real app, it's better to read an access token from an
+  // environement variable or other configuration that's kept outside of
+  // your code base. For this to work, you need to set the
+  // WEB3STORAGE_TOKEN environment variable before you run your code.
+  //   return process.env.WEB3STORAGE_TOKEN;
+}
+
+function makeStorageClient() {
+  return new Web3Storage({ token: getAccessToken() });
+}
 
 export default function App() {
   // const { isInitialized } = useAuth();
-
+  console.log(makeStorageClient());
   return (
     <ThemeConfig>
       <Container maxWidth="md">
