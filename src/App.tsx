b24226982b50633dc76b20b7ffbb8c8627bcdcdd
Mathis <mathis.grosjean.42@gmail.com>
2021-08-05 19:58:26 +0200

    ceramic not workinggggg

--- src/App.tsx
+++ src/App.tsx
@@ -29,6 +29,15 @@ import { Web3Storage } from "web3.storage";
 import { AddFiles } from "./AddFiles";
 import LoadingScreen from "./components/LoadingScreen";
 
+// import { DID } from "dids";
+// import ThreeIdResolver from "@ceramicnetwork/3id-did-resolver";
+// import KeyDidResolver from "key-did-resolver";
+
+// import { createCeramic } from "./ceramic";
+// import { createIDX } from "./idx";
+// import { getProvider } from "./wallet";
+// import type { ResolverRegistry } from "did-resolver";
+
 const DivStyle = styled(motion.div)(({ theme }: { theme: Theme }) => ({
   position: "relative",
   width: "100%",
@@ -94,9 +103,52 @@ function makeStorageClient() {
   return new Web3Storage({ token: getAccessToken() });
 }
 
+// declare global {
+//   interface Window {
+//     did?: DID;
+//   }
+// }
+
+// const ceramicPromise = createCeramic();
+
+// const authenticate = async (): Promise<string> => {
+//   const [ceramic, provider] = await Promise.all([
+//     ceramicPromise,
+//     getProvider(),
+//   ]);
+//   const keyDidResolver = KeyDidResolver.getResolver();
+//   const threeIdResolver = ThreeIdResolver.getResolver(ceramic);
+//   const resolverRegistry: ResolverRegistry = {
+//     ...threeIdResolver,
+//     ...keyDidResolver,
+//   };
+//   const did = new DID({
+//     provider: provider,
+//     resolver: resolverRegistry,
+//   });
+//   await did.authenticate();
+//   await ceramic.setDID(did);
+//   const idx = createIDX(ceramic);
+//   window.did = ceramic.did;
+//   return idx.id;
+// };
+
+const connectWallet = () => {
+  console.log("connnect");
+  // authenticate().then(
+  //   (id) => {
+  //     console.log("Connected with DID:", id);
+  //   },
+  //   (err) => {
+  //     console.error("Failed to authenticate:", err);
+  //   }
+  // );
+};
+
 export default function App() {
   // const { isInitialized } = useAuth();
   console.log(makeStorageClient());
+
   return (
     <ThemeConfig>
       <DivStyle>
@@ -104,7 +156,11 @@ export default function App() {
           <ContentStyle>
             <Card>
               <Typography variant="h4">Connect your wallet</Typography>
-              <Button variant="contained" color="primary">
+              <Button
+                variant="contained"
+                color="primary"
+                onClick={connectWallet}
+              >
                 Connect
               </Button>
             </Card>
--- webpack.dev.config.ts
+++ webpack.dev.config.ts
@@ -35,6 +35,21 @@ const config: webpack.Configuration = {
   },
   resolve: {
     extensions: [".tsx", ".ts", ".js"],
+    fallback: {
+      // tls: false,
+      // net: false,
+      // path: false,
+      // zlib: false,
+      // http: false,
+      // https: false,
+      // stream: false,
+      // os: false,
+      // "crypto-browserify": require.resolve("crypto-browserify"), //if you want to use this module also don't forget npm i crypto-browserify
+      stream: require.resolve("stream-browserify"),
+      http: require.resolve("stream-http"),
+      https: require.resolve("https-browserify"),
+      os: require.resolve("os-browserify/browser"),
+    },
   },
   plugins: [
     new HtmlWebpackPlugin({
